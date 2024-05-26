68b784de9fb265853a0703eb88469512c443acb7
azikodev <aziko.coder@gmail.com>
2024-05-26 19:01:48 +0500

    cuesers

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+//style 
+import './Footer.css'
+
+function Footer() {
+  return ( 
+    <div className='footer'>
+      <div className="footer-container container">
+        <p>
+          All Right Reserved. <a href="https://t.me/azikodev">azikodev</a>
+        </p>
+      </div>
+    </div>
+   );
+}
+
+export default Footer;
